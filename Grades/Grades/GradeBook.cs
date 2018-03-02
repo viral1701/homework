68fba418ae7b208a6b42feec2403dd2621f48a90
Viral Patel <viral.patel@fourth.com>
2018-03-02 16:04:49 +0000

    adding my example

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<float> grades;
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            GradeBook book2 = book;
+            book2.AddGrade(75);
+        }
+    }
+}
