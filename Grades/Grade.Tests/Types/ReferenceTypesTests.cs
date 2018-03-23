617a2c36bcd4b0dbb4ac9734bdc15cf67cfc7ad7
Viral Patel <viral.patel@fourth.com>
2018-03-23 15:13:38 +0000

    working on value types

--- Grades/Grade.Tests/Types/ReferenceTypesTests.cs
+++ Grades/Grade.Tests/Types/ReferenceTypesTests.cs
@@ -9,8 +9,83 @@ using System.Threading.Tasks;
 namespace Grade.Tests.Types
 {
     [TestClass]
-   public class ReferenceTypesTests
+   public class TypesTests
     {
+
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void Uppercasestring()
+        {
+            string name = "viral";
+            name = name.ToUpper();
+
+            Assert.AreEqual("VIRAL", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+            Assert.AreEqual(2, date.Day);
+
+        }
+
+        [TestMethod]
+        public void ValuetypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+
+        [TestMethod]
+        public void StringComparions()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void VariableHoldReference()
         {
